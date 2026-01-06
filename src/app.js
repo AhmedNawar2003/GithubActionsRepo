9ad411fd1283ef39114c90867615684e5f197d3f
Ahmed Nawar <153510555+AhmedNawar2003@users.noreply.github.com>
2026-01-06 20:34:15 +0200

    Test change

--- src/app.js
+++ src/app.js
@@ -2,9 +2,9 @@
     return `Hello, ${name}`
 }
 module.exports= greet;
-// if(require.main === module){
-//     console.log(greet("World"));
-// }
+if(require.main === module){
+    console.log(greet("World"));
+}
 
 
 
